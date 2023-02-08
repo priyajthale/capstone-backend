b7045c0d57a891107e33c4a752a08735a60802ad
sun440 <sun440>
2023-02-08 15:08:23 +0530

    first commit

---
+++ src/main/java/com/project/spring/SpringbootBackendApplication.java
@@ -0,0 +1,14 @@
+package com.project.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+		System.out.println("spring work");
+	}
+
+}
---
+++ src/main/java/com/project/spring/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.project.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.spring.model.Employee;
+import com.project.spring.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+	
+}
